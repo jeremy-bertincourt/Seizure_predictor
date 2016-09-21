8816aead8e6fb1f579f6566e1a1f687e0e5a6e0a
jeremy-bertincourt <bertincourtj@gmail.com>
2016-09-21 18:30:38 +0200

    fix: Select 500 first files instead of 500 first data and take first column for probability predictions

--- predictor.py
+++ predictor.py
@@ -92,12 +92,20 @@ def computeFinalScore(dataLen, MatTestingFiles):
     matPrediction = []
     
     # Read data from HDFStore file 
-    X_train = pd.read_hdf('trainingDataT1.h5', 'data', stop = 500)
-    y_train = pd.read_hdf('trainingDataT1.h5', 'y', stop = 500)
+    X_train = pd.read_hdf('trainingDataT1.h5', 'data', stop = dataLen*500)
+    y_train = pd.read_hdf('trainingDataT1.h5', 'y', stop = dataLen*500)
+
+    print 'X and y train read'
+    
+    X_train = X_train.as_matrix()
+    y_train = np.array(y_train).ravel()
+    y_train = map(int, y_train)
 
     print 'training files processed'
                 
     X_test = pd.read_hdf('testingDataT1.h5', 'data')
+    X_test = X_test.as_matrix()
+    print X_test[0:18000].shape
 
     print 'testing files processed'
 
@@ -128,7 +136,8 @@ def computeFinalScore(dataLen, MatTestingFiles):
     clf = RandomForestClassifier(random_state = 42, n_jobs=8)
     '''
     parameters = {
-        'max_depth': [3],
+        'max_depth': [2, 3, 4],
+        'loss': ['deviance', 'exponential'],
     }
     
     eval_size = 0.10
@@ -136,16 +145,17 @@ def computeFinalScore(dataLen, MatTestingFiles):
 
     scoring_fnc = make_scorer(f1_score)
 
-    clf = GridSearchCV(clf, parameters, scoring_fnc, cv=kf)
+    clf = GridSearchCV(clf, parameters, scoring_fnc, cv=kf, n_jobs=-1)
     '''
     clf.fit(X_train, y_train)
     '''
-    best_parameters, score, _ = max(clf.grid_scores_, key=lambda x: x[1])
-    for param_name in sorted(best_parameters.keys()):
-        print("%s: %r" % (param_name, best_parameters[param_name]))
+    clf = clf.best_estimator_
+
+    print 'best param for max_depth : ', clf.get_params()['max_depth']
+    print 'best param for loss : ', clf.get_params()['loss']
     '''        
     if hasattr(clf, "predict_proba"):
-        prediction = clf.predict_proba(X_test)
+        prediction = clf.predict_proba(X_test)[:, 1]
     else:  # use decision function
         prediction = clf.decision_function(X_test)
         prediction = (prediction - prediction.min()) / (prediction.max() - prediction.min())
@@ -164,20 +174,26 @@ def computeFinalScore(dataLen, MatTestingFiles):
     createSubmission(matPrediction, MatTestingFiles)       	   
 
 
-def computeTestScore(NumberTrainingFiles):           
+def computeTestScore():           
     # Read data from HDFStore file
-    X = read_hdf('trainingDataTestBase.h5', 'data', stop = 500)
-    y = read_hdf('trainingDataTestBase.h5', 'y', stop = 500)
+    X = pd.read_hdf('trainingDataT3.h5', 'data')
+    y = pd.read_hdf('trainingDataT3.h5', 'y')
 
+    print 'X and y read'
+    
+    X = X.as_matrix()
+    y = np.array(y).ravel()
+    y = map(int, y)
+
+    print 'X and y processed'
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-    clf = GradientBoostingClassifier(random_state = 42)
+    clf = RandomForestClassifier(random_state = 42, n_jobs=8)
     clf.fit(X_train, y_train) 
-    if hasattr(clf, "predict_proba"):
-        prob_pos_clf = clf.predict_proba(X_test)[:, 1]
-    else:  # use decision function
-        prob_pos_clf = clf.decision_function(X_test)
-        prob_pos_clf = (prediction - prediction.min()) / (prediction.max() - prediction.min())
+
+    prob_pos_clf = clf.predict_proba(X_test)[:, 1]
+
     '''
     # Model with isotonic calibration
     clf_isotonic = CalibratedClassifierCV(clf, cv=2, method='isotonic')
@@ -217,117 +233,118 @@ def processData(MatFiles, dataType):
     init = 0
 
     if dataType == 'training':  
-        hf = pd.HDFStore('trainingDataT1.h5', mode='w')
+        hf = pd.HDFStore('trainingDataT3test.h5', mode='w')
     else:
-        hf = pd.HDFStore('testingDataT1.h5', mode='w')
+        hf = pd.HDFStore('testingDataT3test.h5', mode='w')
     
     # Obtain data
     for MatFile in MatFiles:
-            data = None
-            y = None 
-            try:
-                    d = loadmat(MatFile)
-            except ValueError as ex:
-                    print(u'Error loading MAT file {}: {}'.format(os.path.basename (MatFile), str(ex)))
-                    continue
-  
-            # Extract each data from the file
-            datastruct = d['dataStruct']
-            channelIndices = datastruct['channelIndices'][0][0][0]
-            iEEGsamplingRate = datastruct['iEEGsamplingRate'][0][0][0][0]
-            nSamplesSegment = datastruct['nSamplesSegment'][0][0][0][0]
-            ieegData = datastruct['data'][0][0].transpose()
+        data = None
+        y = None 
+        try:
+            d = loadmat(MatFile)
+        except ValueError as ex:
+            print(u'Error loading MAT file {}: {}'.format(os.path.basename (MatFile), str(ex)))
+            continue
 
-            segmentEnd = 30
+        # Extract each data from the file
+        datastruct = d['dataStruct']
+        channelIndices = datastruct['channelIndices'][0][0][0]
+        iEEGsamplingRate = datastruct['iEEGsamplingRate'][0][0][0][0]
+        nSamplesSegment = datastruct['nSamplesSegment'][0][0][0][0]
+        ieegData = datastruct['data'][0][0].transpose()
 
-            # Extract labels
-            if dataType == 'training': 
-                # extract the class of the file
-                header = re.split(r"[_.]", os.path.basename (MatFile))
-                category = header[2]
-                subY = np.repeat(category, nSamplesSegment/(iEEGsamplingRate/segmentEnd), axis=0)
-                subY = pd.DataFrame({'y': subY})
+        segmentEnd = 30
 
-            # Frequency vector
-            f = iEEGsamplingRate*np.linspace(0,nSamplesSegment/(iEEGsamplingRate/segmentEnd),nSamplesSegment/(iEEGsamplingRate/segmentEnd))/nSamplesSegment                 
+        # Extract labels
+        if dataType == 'training': 
+            # extract the class of the file
+            header = re.split(r"[_.]", os.path.basename (MatFile))
+            category = header[2]
+            subY = np.repeat(category, nSamplesSegment/(iEEGsamplingRate/segmentEnd), axis=0)
+            subY = pd.DataFrame({'y': subY})
 
-            nElectrodes = len(channelIndices)
-            
-            for el in range(nElectrodes):
+        # Frequency vector
+        f = iEEGsamplingRate*np.linspace(0,nSamplesSegment/(iEEGsamplingRate/segmentEnd),nSamplesSegment/(iEEGsamplingRate/segmentEnd))/nSamplesSegment                 
 
-                Y = np.fft.fft(ieegData[el])
+        nElectrodes = len(channelIndices)
+        
+        for el in range(nElectrodes):
 
-                filtered = []
-                b= []               # store filter coefficient
-                cutoff = [0.001, 0.5, 3.0, 8.0, 12.0, 30.0]
+            Y = np.fft.fft(ieegData[el])
 
-                for band in xrange(0, len(cutoff)-1):
-                    wl = 2*cutoff[band]/iEEGsamplingRate*np.pi
-                    wh = 2*cutoff[band+1]/iEEGsamplingRate*np.pi
-                    M = 512      # Set number of weights as 128
-                    bn = np.zeros(M)
+            filtered = []
+            b= []               # store filter coefficient
+            cutoff = [0.001, 0.5, 3.0, 8.0, 12.0, 30.0]
 
-                    for i in xrange(0,M):     # Generate bandpass weighting function
-                        n = i - M/2       # Make symmetrical
-                        if n == 0:
-                            bn[i] = wh/np.pi - wl/np.pi;
-                        else:
-                            bn[i] = (np.sin(wh*n))/(np.pi*n) - (np.sin(wl*n))/(np.pi*n)   # Filter impulse response
-                    
-                    
-                    bn = bn*np.kaiser(M,5.2)  # apply Kaiser window, alpha= 5.2
-                    b.append(bn)
+            for band in xrange(0, len(cutoff)-1):
+                wl = 2*cutoff[band]/iEEGsamplingRate*np.pi
+                wh = 2*cutoff[band+1]/iEEGsamplingRate*np.pi
+                M = 512      # Set number of weights as 128
+                bn = np.zeros(M)
 
-                    [w,h]=freqz(bn,1)
-                    filtered.append(np.convolve(bn, ieegData[el])) # filter the signal by convolving the signal with filter coefficients
-                '''
-                plt.subplot(16, 1, el+1)
-                plt.plot(ieegData[el])
-                for i in xrange(0, len(filtered)):
-                    y_p = filtered[i]
-                    plt.plot(y_p[ M/2:nSamplesSegment+M/2])
-                plt.axis('tight')
-                plt.xlabel('Time (seconds)')
-                '''
-                #ax = plt.subplot(nElectrodes, 1, el+1)
-                #plt.plot(f,2*np.abs(Y[0:nSamplesSegment/(iEEGsamplingRate/segmentEnd)]))
-                for i in xrange(0, len(filtered)):
-                    Y = filtered[i]
-                    Y = np.fft.fft(Y[M/2:nSamplesSegment+M/2])
-                    subData = abs(Y[0:nSamplesSegment/(iEEGsamplingRate/segmentEnd)])
-                    subFrame = pd.DataFrame({'%d_%d' % (el, i): subData})
-                    if data is None:
-                        data = subFrame
+                for i in xrange(0,M):     # Generate bandpass weighting function
+                    n = i - M/2       # Make symmetrical
+                    if n == 0:
+                        bn[i] = wh/np.pi - wl/np.pi;
                     else:
-                        data = data.join(subFrame)
-                        
-                    #plt.plot(f,subData)
-                    #plt.axis('tight')
-                    #plt.axis([0, 30, 0, 300000])
-                    #ax.set_autoscale_on(False)
-            #print data.shape     
-            #plt.legend(['infra band, 0.0-0.5 Hz', 'delta band, 0.5-3 Hz','theta band, 3-8 Hz','alpha band, 8-12 Hz','beta band, 12-30 Hz'])
+                        bn[i] = (np.sin(wh*n))/(np.pi*n) - (np.sin(wl*n))/(np.pi*n)   # Filter impulse response
                 
-            #plt.show()         
+                
+                bn = bn*np.kaiser(M,5.2)  # apply Kaiser window, alpha= 5.2
+                b.append(bn)
 
-            # Write in HDFStore file the processed data
-            if init == 0:
-                hf.put('data', data, format='table')
-                dataLenght = data.shape[0]
-            else:
-                hf.append('data', data, format='table')
-            
-            if dataType == 'training':
-                if init == 0:
-                    hf.put('y', subY, format='table')
+                [w,h]=freqz(bn,1)
+                filtered.append(np.convolve(bn, ieegData[el])) # filter the signal by convolving the signal with filter coefficients
+            '''
+            plt.subplot(16, 1, el+1)
+            plt.plot(ieegData[el])
+            for i in xrange(0, len(filtered)):
+                y_p = filtered[i]
+                plt.plot(y_p[ M/2:nSamplesSegment+M/2])
+            plt.axis('tight')
+            plt.xlabel('Time (seconds)')
+            '''
+            #ax = plt.subplot(nElectrodes, 1, el+1)
+            #plt.plot(f,2*np.abs(Y[0:nSamplesSegment/(iEEGsamplingRate/segmentEnd)]))
+            for i in xrange(0, len(filtered)):
+                Y = filtered[i]
+                Y = np.fft.fft(Y[M/2:nSamplesSegment+M/2])
+                subData = abs(Y[0:nSamplesSegment/(iEEGsamplingRate/segmentEnd)])
+                subFrame = pd.DataFrame({'%d_%d' % (el, i): subData})
+                if data is None:
+                    data = subFrame
                 else:
-                    hf.append('y', subY, format='table')
+                    data = data.join(subFrame)
+                    
+                #plt.plot(f,subData)
+                #plt.axis('tight')
+                #plt.axis([0, 30, 0, 300000])
+                #ax.set_autoscale_on(False)
+        #print data.shape     
+        #plt.legend(['infra band, 0.0-0.5 Hz', 'delta band, 0.5-3 Hz','theta band, 3-8 Hz','alpha band, 8-12 Hz','beta band, 12-30 Hz'])
             
-            fileNumber += 1
-            init = 1
-            
-            if fileNumber % 10 == 0.0:
-                print '%d files done' % fileNumber
+        #plt.show()         
+
+        # Write in HDFStore file the processed data
+        if init == 0:
+            hf.put('data', data, format='table')
+            dataLenght = data.shape[0]
+            print data.shape
+        else:
+            hf.append('data', data, format='table')
+        
+        if dataType == 'training':
+            if init == 0:
+                hf.put('y', subY, format='table')
+            else:
+                hf.append('y', subY, format='table')
+        
+        fileNumber += 1
+        init = 1
+        
+        if fileNumber % 10 == 0.0:
+            print '%d files done' % fileNumber
             
     hf.close()
     
@@ -340,11 +357,11 @@ def processData(MatFiles, dataType):
 
 
 if __name__=="__main__":
-    #MatTrainingFiles= getFiles("train_1")
+    #MatTrainingFiles= getFiles("train_2")
     MatTestingFiles= getFiles("test_1")
     #NumberTrainingFiles, dataTrainingLenght = processData(MatTrainingFiles, 'training')
     #NumberTestingFiles, dataTestingLenght = processData(MatTestingFiles, 'testing')
 
     computeFinalScore(18000, MatTestingFiles)
-    #computeTestScore(250)
+    #computeTestScore()
 
